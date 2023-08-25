aaa042e394c487e869d6ef97084e1b1201fd8e80
Daniel Wolf <wolf.dani9@gmail.com>
2023-08-25 10:58:26 +0200

    watchers and code reformat

--- vue-essentials/app.js
+++ vue-essentials/app.js
@@ -25,6 +25,13 @@ const vm = Vue.createApp({
     fullName() {
       return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`;
     },
+  },
+  watch: {
+    age(newVal, oldVal) {
+      setTimeout(() => {
+        this.age = 20
+      }, 3000)
+    }
   }
 }).mount("#app");
 
